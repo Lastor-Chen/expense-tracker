8acaf9826ca782e49123a97b889614e82c5f426b
Lastor <lasotraya@gmail.com>
2019-09-18 17:27:16 +0800

    [加入] lib，getCategoryIcon 取得類別icon

--- routes/home.js
+++ routes/home.js
@@ -7,13 +7,7 @@ const express = require('express')
 const router = express.Router()
 
 const Recode = require('../models/record.js')
-const categoryMap = {
-  '家居物業': 'home',
-  '交通出行': 'shuttle-van',
-  '休閒娛樂': 'grin-beam',
-  '餐飲食品': 'utensils',
-  '其他': 'pen'
-}
+const lib = require('../lib/lib.js')
 
 // routes '/'
 // ==============================
@@ -24,12 +18,8 @@ router.get('/index', (req,res) => {
   Recode.find((err, records) => {
     if (err) console.error(err)
 
-    records.forEach(record => {
-      // 取得 category icon
-      for (const key in categoryMap) {
-        if (record.category === key) record.category = categoryMap[key]
-      }
-    })
+    // 取得 font-awesome icon 名稱
+    lib.getCategoryIcon(records)
 
     res.render('index', { css: 'index', records })
   })
