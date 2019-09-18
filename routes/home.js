1d13941082fc4f2ff1d1325632fd19f1e834011c
Lastor <lasotraya@gmail.com>
2019-09-18 17:04:54 +0800

    [修改] "/index" controller，從db抓資料

--- routes/home.js
+++ routes/home.js
@@ -7,14 +7,13 @@ const express = require('express')
 const router = express.Router()
 
 const Recode = require('../models/record.js')
-// const records = require('../models/seeds/recode.json')
-// const categoryMap = {
-//   '家居物業': 'home',
-//   '交通出行': 'shuttle-van',
-//   '休閒娛樂': 'grin-beam',
-//   '餐飲食品': 'utensils',
-//   '其他': 'pen'
-// }
+const categoryMap = {
+  '家居物業': 'home',
+  '交通出行': 'shuttle-van',
+  '休閒娛樂': 'grin-beam',
+  '餐飲食品': 'utensils',
+  '其他': 'pen'
+}
 
 // routes '/'
 // ==============================
@@ -24,17 +23,16 @@ router.get('/', (req, res) => res.redirect('/index'))
 router.get('/index', (req,res) => {
   Recode.find((err, records) => {
     if (err) console.error(err)
-    console.log(records[0].date.toLocaleString())
+
+    records.forEach(record => {
+      // 取得 category icon
+      for (const key in categoryMap) {
+        if (record.category === key) record.category = categoryMap[key]
+      }
+    })
 
     res.render('index', { css: 'index', records })
   })
-
-  // records.forEach(record => {
-  //   for (const cat in categoryMap) {
-  //     if (record.category === cat) record.category = categoryMap[cat]
-  //   }
-  // })
-
 })
 
 
