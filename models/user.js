4a442f211fe432be4c9c78e110e84e9e021d7f39
Lastor <lasotraya@gmail.com>
2019-09-19 17:12:56 +0800

    [加入] user Model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+// models user.js
+
+// Model setting
+// ==============================
+
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+// export
+// ==============================
+
+module.exports = mongoose.model('User', userSchema)
