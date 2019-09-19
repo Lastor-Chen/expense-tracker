91c2e5156bdca7912c06926e8ee6db250f471aa7
Lastor <lasotraya@gmail.com>
2019-09-19 16:02:33 +0800

    [重構] 拆分 index 頁面之 JS 作為 lib.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,10 @@
 // index 頁面附屬 js
 
+// import module
+// =================================
+import { showSelected, setOddEven, getSum } from './lib.js'
+
+
 // 刪除確認
 // =================================
 
@@ -20,30 +25,11 @@ $('#filter').on('input', e => {
   const keyword = e.target.value
 
   // 只顯示被選中的 category
-  $('[data-cate]').each( (index, elem) => {
-    const category = elem.dataset.cate
+  showSelected(keyword)
 
-    // elem.hidden 帶 false 將導致迭代中斷，故改用 jquery
-    if (keyword === 'all') return $(elem).attr('hidden', false)
-    
-    if (category !== keyword) { elem.hidden = true }
-    if (category === keyword) { elem.hidden = false }
-  })
+  // 重設 visible record 之 classList
+  setOddEven()
 
-  // 重設 visible record 之 UI style
-  let times = 1
-  $('[data-cate]:visible').each( (index, elem) => {
-    elem.classList.remove('odd')
-
-    if (times % 2 === 1) { elem.classList.add('odd') }
-    times++
-  })
-
-  // 計算總金額
-  let sum = 0
-  $('[data-cate]:visible .amount').each( (index, elem) => {
-    sum += (+elem.textContent)
-  })
-
-  $('#sum').html(sum)
+  // 計算總金額，顯示於頁面
+  getSum()
 })
